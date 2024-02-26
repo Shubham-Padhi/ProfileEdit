51db1a47b3bd5ed9d5d7e4e5e1f9202500618e1a
Shubham Padhi <shubhampadhi404@gmail.com>
2024-02-26 19:45:36 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/setting',function(req,res,next){
+  res.render('setting');
+})
+router.get("/about",function (req,res,next){
+  res.render("about");
+})
+
+
+module.exports = router;
